185249634febb834a224e918a9159d712afc0efd
Sajid Shahariar <fahimshariar511@gmail.com>
2022-05-01 18:12:42 +0600

    pagination

--- index.js
+++ index.js
@@ -49,6 +49,11 @@ async function run() {
             res.send(products);
         });
 
+        app.get('/carsCount', async (req, res) => {
+            const count = await carsCollection.estimatedDocumentCount();
+            res.send({ count });
+        })
+
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
