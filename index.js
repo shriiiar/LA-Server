a396b1374df74587f3f24e98c5073b57e7f8a3cb
Sajid Shahariar <fahimshariar511@gmail.com>
2022-05-01 15:58:50 +0600

    implementing jwt

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.vftdj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -67,9 +83,8 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/myItems', async (req, res) => {
+        app.get('/myItems',  async (req, res) => {
             const email = req.query.email;
-            console.log(email);
             const query = { email: email };
             const cursor = carsCollection.find(query);
             const myItems = await cursor.toArray();
